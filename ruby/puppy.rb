47b5ad7861bb482ffcedc3a3ae125561a727f442
eitanben <eoa1901@gmail.com>
2017-07-06 13:15:20 -0700

    6.1 release o

---
+++ ruby/puppy.rb
@@ -0,0 +1,47 @@
+# 6.1 - Introduction to Classes.  Release 0
+Puppies ------------------------ Most popular breeds in the USA as 2017
+Labrador
+German Shepherd
+Golden Retriever
+Bulldog
+Beagle
+Poodle
+Rottweiler
+Terrier
+Boxer
+Chihuahua
+French Bulldog
+Pug
+Irish Setter
+
+CHARACTERISTICS ----------------
+Group: Hound, Herding, Terrier, Sporting, Urban
+Size: Large, Medium, Small
+Friendly: Y/N
+Apartment Dog: Y/N
+Age:
+Guard Dog: Y/N
+Hypoallergenic Dog: Y/N
+Hairless Dog: Y/N
+Good for Kids:Y/N
+Family Dog: Y/N
+Hair Length: Long, Medium, Short
+Name:
+Color:
+Weight: Heavy, Medium, Light
+Life Expentancy:
+BEHAVIOR ----------------------- Y/N flags and 1-5 scale determines the behavior
+Barker:
+Confident:
+Dominant:
+Trainable:
+Aggression:
+Anxiety:
+Protective:
+Sensitive:
+Loyal:
+Noisy:
+Obedient:
+Energetic:
+Play Ball:
+Nervous:
